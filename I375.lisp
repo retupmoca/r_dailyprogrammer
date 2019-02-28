5f28d8297561aef9e26b8e689fae027842b2b885
Andrew Egeler <andrew@egeler.us>
2019-02-27 22:39:47 -0500

    add a (slow) I375.lisp

---
+++ I375.lisp
@@ -0,0 +1,69 @@
+; this algorithm works, but it's not particularly fast
+; which makes sense, given that this is just a brute-force search
+
+(defun bitflip (val)
+  (if (= val 1)
+      0
+      1))
+
+(defun is-1 (val)
+  (and (not (null val))
+       (= val 1)))
+
+(defmacro def-spotfun (name &body body)
+  `(defun ,name (idx cards)
+     $!card (nth idx cards)
+     ,@body))
+
+(def-spotfun flip-card
+  (when (>= idx 0)
+    (when (not (null card))
+      (setf card (bitflip card)))))
+
+(def-spotfun remove-card
+  (when (not (is-1 card))
+    (error "Can't flip this position"))
+  (setf card nil)
+  (flip-card (1- idx) cards)
+  (flip-card (1+ idx) cards)
+  cards)
+
+(defun unsolvable-p (list)
+  $island-1 nil
+  $in-island nil
+  $#island-fails () (if in-island
+                        (if island-1
+                            (clearf island-1 in-island)
+                            t)
+                        nil)
+  (dolist (x list)
+        (if (null x)
+            (when (island-fails)
+              (return t))
+            (markf in-island))
+        (when (is-1 x)
+          (markf island-1)))
+  (island-fails))
+
+(defun try-removes (removes cards)
+  $my-cards (copy-list cards)
+  (dolist (remove removes)
+    (remove-card remove my-cards)
+    (when (unsolvable-p my-cards)
+      (error "Cards are now unsolvable!")))
+  my-cards)
+
+(defun brute-force-solve (cards &optional pre)
+  (print pre)
+  (iter (for i from 0 to (length cards))
+        $next-pre (append (copy-list pre) (list i))
+        (handler-case
+            (try-removes next-pre cards)
+          (simple-error () (print (cons "failed" next-pre)) (next-iteration)))
+        (print (cons "success" next-pre))
+        (if (= (length cards) (length next-pre))
+            (return-from brute-force-solve next-pre)
+            $recret (brute-force-solve cards next-pre)
+            (when (not (null recret))
+              (return-from brute-force-solve recret))))
+  nil)
